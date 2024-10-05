31cde7544619d12fca12ebd4bdc5defc193573aa
Rohit Kada <rohitkadam2425@gmail.com>
2024-10-05 21:27:17 +0530

    Minor changes

--- src/app/components/work/work.jsx
+++ src/app/components/work/work.jsx
@@ -9,10 +9,10 @@ function  Work() {
   return (
   
     <div className='h-[auto] mt-5 ml-5 mr-5 mt-10'>
-       <h2 className='text-3xl font-black font-monospace text-gray-300 mb-4'>Works</h2>
+       <h2 className='text-3xl font-black font-cursive text-gray-300 mb-4'>Works</h2>
       <hr></hr>
       <h4 className='mb-4 font-monospace'>Projects I have Worked On</h4>
-      <Grid container>
+      <Grid container className='flex justify-around'>
         <WorkCard image={rake} name={"Rake Casino"} url= {"https://rake.com/"}/>
         <WorkCard image={stoxbox} name={"Stox Box"}  url= {"https://stoxbox.in/trading-stoxbox"}/>
         <WorkCard image={preschool} name={"Wingham School"}  url= {"https://preschool-green.vercel.app/login"}/>
